3ef473f7b30d0c6027d4f190994393783cd82d8b
Mario Ramírez <marioh.ramirez@udea.edu.co>
2024-03-27 13:33:43 -0500

    Configuracion inicial de Login

---
+++ src/component/Auth/Auth.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Modal } from "@mui/material";
+
+export const Auth = () => {
+  return (
+    <>
+      <Modal >
+
+      </Modal>
+    </>
+  );
+};
+
+export default Auth;
---
+++ src/component/Auth/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Modal } from "@mui/material";
+
+export const LoginForm = () => {
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default LoginForm;
