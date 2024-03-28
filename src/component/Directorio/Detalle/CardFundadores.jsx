881857c6ac7e805b40c42e2e580332cdf4e6b75b
Mario Ramírez <marioh.ramirez@udea.edu.co>
2024-03-28 03:13:06 -0500

    Agregadas funcionalidades a cards de la parte de emprendimientos

--- src/component/Directorio/Detalle/CardFundadores.jsx
+++ src/component/Directorio/Detalle/CardFundadores.jsx
@@ -1,7 +1,10 @@
 import { Card } from "@mui/material";
 import React from "react";
-import { WhatsAppIcon, LinkedIcon } from "../../Iconos";
+import { LinkedIcon } from "../../Iconos";
+import { motion } from "framer-motion";
+
 const fundadores = [1, 1, 1];
+
 const CardFundadores = () => {
   return (
     <Card
@@ -14,7 +17,7 @@ const CardFundadores = () => {
       <div className="bg-[#026937] p-4 textPart lg:flex flex-col gap-5">
         {fundadores.map((item) => (
           <div className="flex flex-row justify-center items-center gap-5">
-            <div className=" bg-slate-500 rounded-full w-[50px] h-[50px]">
+            <div className=" bg-slate-500 rounded-full w-[55px] h-[55px]">
               <img
                 className="rounded-t-md"
                 src="/png/Logos/logoipsum-300.png"
@@ -24,9 +27,17 @@ const CardFundadores = () => {
             <div className="flex flex-col">
               <p className="font-semibold">Nombre del fundador</p>
               <p>Cargo</p>
-              <span className=" w-5">
-                <LinkedIcon />
-              </span>
+              <nav className="flex flex-row gap-10">
+                <motion.a
+                  href="https://www.linkedin.com"
+                  target="_blank"
+                  whileHover={{ y: -2 }}
+                  whileTap={{ scale: 0.9 }}
+                  className="w-7"
+                >
+                  <LinkedIcon />
+                </motion.a>
+              </nav>
             </div>
           </div>
         ))}
--- src/component/Directorio/EmprendimientoCard.jsx
+++ src/component/Directorio/EmprendimientoCard.jsx
@@ -1,8 +1,11 @@
-import { Card, CardContent, Chip, Link } from "@mui/material";
+import { Card } from "@mui/material";
 import React from "react";
 import { WhatsAppIcon, LinkedIcon } from "../Iconos";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const EmprendimientoCard = () => {
+  const navigate = useNavigate();
   return (
     <Card
       className="shadow-md rounded-lg  w-[18rem] "
@@ -18,17 +21,34 @@ const EmprendimientoCard = () => {
       <div className="bg-[#026937] p-4 textPart lg:flex flex-col justify-between items-center">
         <p className="font-semibold text-lg">Nombre de la empresa</p>
         <p className="text-sm">Informacion de empresa</p>
-        <div className="flex flex-row gap-10 pt-2">
-            <span className=" w-10">
-              <WhatsAppIcon />
-            </span>
-            <span className=" w-10">
-              <LinkedIcon/>
-            </span>
-        </div>
+        <nav className="flex flex-row gap-10 pt-2">
+          <motion.a
+            href="https://wa.me/573002222222"
+            target="_blank"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-9"
+          >
+            <WhatsAppIcon />
+          </motion.a>
+          <motion.a
+            href="https://www.linkedin.com"
+            target="_blank"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-9"
+          >
+            <LinkedIcon />
+          </motion.a>
+        </nav>
       </div>
-      <div className=" bg-blue-500 p-2 lg:flex flex-col justify-center items-center">
-        <p>Ver mas</p>
+      <div className=" bg-[#137598] p-2 lg:flex flex-col justify-center items-center">
+        <p
+          onClick={() => navigate("/directorio/detalle")}
+          className=" cursor-pointer hover:text-black"
+        >
+          Ver mas
+        </p>
       </div>
     </Card>
   );
