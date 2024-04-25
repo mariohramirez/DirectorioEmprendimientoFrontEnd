67fdb73f6c25f14ded253ad1a8e9d2201e7df590
Mario Ramírez <marioh.ramirez@udea.edu.co>
2024-04-25 04:58:09 -0500

    Cambios iniciales a la nueva interfaz de novedades

---
+++ src/component/Noticias/Carousel.jsx
@@ -0,0 +1,91 @@
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+import React, { useEffect, useState } from "react";
+
+function Carousel({ children: slides, autoSlide = false, autoSlideInterval = 3000}) {
+  const [curr, setCurrent] = useState(0);
+
+  const prev = () =>
+    setCurrent((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
+  const next = () =>
+    setCurrent((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
+
+    useEffect(() => {
+        if (!autoSlide) return
+        const slideInterval = setInterval(next, autoSlideInterval)
+        return () => clearInterval(slideInterval)
+    },[])
+
+  return (
+    <div className="w-[80vw] z-10">
+      <div className="mt-10  text-white">
+        <div className="bg-[#026937] font-bold lg:text-2xl p-2 rounded-t-md">
+          <span>Novedades</span>
+        </div>
+        <div className=" font-bold lg: relative flex flex-col justify-center items-center rounded-b-md">
+          <div>
+            <div className="overflow-hidden relative">
+              <div
+                className="flex transition-trasnform ease-out duration-500"
+                style={{ transform: `translateX(-${curr * 100}%)` }}
+              >
+                {slides}
+              </div>
+              <div className="absolute inset-0 flex items-center justify-between p-4">
+                <button
+                  onClick={prev}
+                  className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
+                >
+                  <ChevronLeft size={40} />
+                </button>
+                <button
+                  onClick={next}
+                  className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
+                >
+                  <ChevronRight size={40} />
+                </button>
+              </div>
+              <div className="absolute bottom-4 right-0 left-0">
+                <div className="flex items-center justify-center gap-2">
+                    {slides.map((_, index) => (<div className={`transition-all w-3 h-3 bg-white rounded-full
+                    ${curr===index? "p-2" : "bg-opacity-50"}`}/>))}
+                </div>
+              </div>
+            </div>
+            <img
+              className="w-[80vw] h-[20rem] object-cover z-10"
+              src="/jpg/FotosMiniatura/fotoNoticia.jpg"
+              alt="Foto"
+            />
+            <div className="absolute inset-y-[0rem] inset-x-[0rem] bg-[#026937] bg-opacity-50 w-full -z-10">
+              <div
+                className="absolute bg-white rounded-full w-[3rem] h-[3rem]
+                   text-black inset-x-[2rem] inset-y-[9rem]"
+              >
+                Boton
+              </div>
+              <span className="text-5xl">Titulo novedad</span>
+              <div
+                className="absolute bg-white rounded-full w-[3rem] h-[3rem] inset-y-[9rem] inset-x-[15rem]
+                   text-black lg: left-[60rem] right-96 end-4"
+              >
+                Boton
+              </div>
+              <div>Estados</div>
+            </div>
+          </div>
+          <div className=" bg-[#026937] bg-opacity-50 py-4 text-center rounded-b-md lg:w-[80vw] px-10 z-20">
+            <span className=" text-sm w-[60vw]">
+              Lorem ipsum dolor sit amet consectetur. Leo sollicitudin ante
+              curabitur accumsan sodales nibh. Sed purus tellus id aenean
+              volutpat. Diam turpis convallis at ut fringilla elementum ornare
+              sit. Viverra sagittis sapien mauris nibh. At tincidunt gravida
+              dolor aliquet magna in praesent.{" "}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
--- src/component/Noticias/Noticias.jsx
+++ src/component/Noticias/Noticias.jsx
@@ -1,31 +1,64 @@
 import React from "react";
 import FiltroNoticia from "./FiltroNoticia";
 import CardNoticia from "./CardNoticia";
+import Carousel from "./Carousel";
+import ListaNoticia from "./ListaNoticia";
 
 {
   /*Arreglo de emprendimientos a partir de los cuales se crean las cards*/
 }
+const slides = [
+  "/jpg/FotosMiniatura/fotoNoticia.jpg",
+  "/jpg/FotosMiniatura/fotoNoticia.jpg",
+  "/jpg/FotosMiniatura/fotoNoticia.jpg",
+];
 const noticias = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 const filtro = [1, 1];
 const Noticias = () => {
   return (
     <div className="">
+      <section className="relative flex flex-col justify-center items-center">
+        <Carousel autoSlide={true}>
+          {slides.map((slide) => (
+            <img className="w-[80vw]" src={slide} />
+          ))}
+        </Carousel>
+        <div
+          className=" p-5 my-10 flex flex-col justify-center items-center 
+        text-[#026937] text-4xl font-bold border-t-2 border-b-2 border-[#026937] w-[80vw]"
+        >
+          <div className="flex flex-row  justify-evenly">
+            <div className=" border-r-2 border-[#026937] px-10">
+              <span className="hover:border-b-2 border-[#026937] cursor-pointer">
+                Todas
+              </span>
+            </div>
+            <div className=" border-r-2 border-[#026937] px-10 ">
+              <span className="hover:border-b-2 border-[#026937] cursor-pointer">
+                Novedades
+              </span>
+            </div>
+            <span className=" border-r-2 border-[#026937] px-10">
+              <span className="hover:border-b-2 border-[#026937] cursor-pointer">
+                Convocatorias
+              </span>
+            </span>
+            <div className=" px-10">
+              <span className="hover:border-b-2 border-[#026937] cursor-pointer">
+                Eventos
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
       {/*Seccion con las card de los emprendimientos*/}
       <section className="relative flex flex-col justify-center items-center">
-        <div className="flex flex-wrap items-center justify-around gap-5 pt-10 w-[80vw] text-black">
-          {
-            /*Mapeo de los filtros de los emprendimientos*/
-            filtro.map((item) => (
-              <FiltroNoticia />
-            ))
-          }
-        </div>
         {/*Div realizado para la totalidad de las cartas*/}
-        <div className="flex flex-wrap items-center justify-around gap-5 py-10 px-[50px]">
+        <div className="flex flex-col items-center justify-around gap-5 py-10 px-[50px]">
           {
             /*Mapeo de las noticias donde cada item crea una card de noticia*/
             noticias.map((item) => (
-              <CardNoticia />
+              <ListaNoticia />
             ))
           }
         </div>
