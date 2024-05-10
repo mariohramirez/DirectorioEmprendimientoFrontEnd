26df21fc53175f45b45843a9d1edb4b991aac9e6
Mario Ramírez <marioh.ramirez@udea.edu.co>
2024-05-10 04:38:31 -0500

    Cambios menores

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,35 +1,23 @@
 import React from "react";
 import RoleButton from "./RoleButton";
+import { infoHome} from "./data";
 
 const Home = () => {
   return (
     <div className="">
       <section className="banner relative flex flex-col justify-center items-center">
-        <div className="w-[80vw] z-10 text-center">
-          <p className="text-2xl text-black lg:text-5xl font-bold z-10 py-10">
+        <div className="w-[80vw] z-10">
+          <p className="text-2xl text-black lg:text-5xl font-bold z-10 py-10 text-center">
             Unidad de innovación de la Facultad de ingenieria
           </p>
-          <p className="z-10 text-black lg:text-m">
-            Lorem ipsum dolor sit amet consectetur. Sed enim sem ullamcorper
-            dictumst feugiat gravida adipiscing eu purus. Eget sit odio faucibus
-            aliquam dictumst arcu. Diam nunc tincidunt tincidunt nec vitae.
-            Aliquam turpis enim magna sed eu etiam purus congue. Est sit sed a
-            massa. In nisi ultrices vulputate pulvinar in sed adipiscing semper.
-            Nulla porttitor vestibulum tincidunt ut amet magna. Morbi at diam
-            cras et sit nisl ultrices. Cursus tristique ultricies a ultrices nam
-            velit elit sollicitudin. Pellentesque suscipit nulla maecenas eu et.
-            Etiam eu mus aliquam tortor bibendum senectus neque est. Sapien
-            viverra ut vitae amet vitae. Eu libero adipiscing rutrum elementum
-            sem nulla rutrum. Dui cras mi eu nibh diam vitae aenean elit. Sed
-            sit cursus quis ut faucibus vitae praesent velit. Lectus augue
-            viverra sit gravida neque. Convallis cursus ipsum in velit. Orci
-            vitae quam sed donec rhoncus egestas. Ultricies iaculis sagittis
-            convallis tellus lacus. Quisque fermentum dictumst aliquam justo
-            nisi erat sed. Pellentesque amet eget ac lacus faucibus nunc eu.
-            Ullamcorper id arcu maecenas dignissim montes.
+          <p className="z-10 text-black lg:text-m text-justify">
+            {infoHome[0].informacion}
           </p>
+          <div className="mt-10">
+            <img src={infoHome[0].imagen} alt={infoHome.title} className="w-[100%] h-[100%] object-cover" />
+          </div>
           <div className="bg-[#3EBDAC] py-[80px] mt-10">
-            <span className="font-bold lg:text-5xl">
+            <span className="font-bold lg:text-5xl ">
               Ruta del emprendimiento
             </span>
           </div>
---
+++ src/component/Home/data.js
@@ -0,0 +1,9 @@
+export const infoHome = [
+    {
+        id: 1,
+        title: "Informacion Inicio",
+        informacion: "Lorem ipsum dolor sit amet consectetur. Sed enim sem ullamcorper dictumst feugiat gravida adipiscing eu purus. Eget sit odio faucibus aliquam dictumst arcu. Diam nunc tincidunt tincidunt nec vitae. Aliquam turpis enim magna sed eu etiam purus congue. Est sit sed a massa. In nisi ultrices vulputate pulvinar in sed adipiscing semper. Nulla porttitor vestibulum tincidunt ut amet magna. Morbi at diam cras et sit nisl ultrices. Cursus tristique ultricies a ultrices nam velit elit sollicitudin. Pellentesque suscipit nulla maecenas eu et. Etiam eu mus aliquam tortor bibendum senectus neque est. Sapien viverra ut vitae amet vitae. Eu libero adipiscing rutrum elementum sem nulla rutrum. Dui cras mi eu nibh diam vitae aenean elit. Sed sit cursus quis ut faucibus vitae praesent velit. Lectus augue viverra sit gravida neque. Convallis cursus ipsum in velit. Orci vitae quam sed donec rhoncus egestas. Ultricies iaculis sagittis convallis tellus lacus. Quisque fermentum dictumst aliquam justo nisi erat sed. Pellentesque amet eget ac lacus faucibus nunc eu. Ullamcorper id arcu maecenas dignissim montes.",
+        imagen: "https://concepto.de/wp-content/uploads/2020/11/emprendimiento-e1676646759170-800x400.jpg",
+        fecha: "2024-05-10",
+    }
+]
